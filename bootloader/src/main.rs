a82f2935023a2356205c651fa755fe653db5477c
hmiya6 <h.miya.work@gmail.com>
2021-11-21 21:37:26 +0900

    add efi_main and panic handler for bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+#[no_mangle]
+pub extern "efiapi" fn efi_main(
+    image: uefi::Handle,
+    mut system_table: uefi::table::SystemTable<uefi::table::Boot>,
+) -> ! {
+    // println!("Hello, world!");
+    loop {}
+}
+
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
