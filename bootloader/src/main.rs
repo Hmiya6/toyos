3f72ab44e9dc76ca8d166c232cdcfb705f897594
hmiya6 <h.miya.work@gmail.com>
2021-11-22 23:27:03 +0900

    Hello UEFI

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -8,6 +8,10 @@ pub extern "efiapi" fn efi_main(
     mut system_table: uefi::table::SystemTable<uefi::table::Boot>,
 ) -> ! {
     // println!("Hello, world!");
+    // use uefi::prelude::ResultExt;
+    use core::fmt::Write;
+    system_table.stdout().clear().unwrap();
+    writeln!(system_table.stdout(), "Hello bootloader").unwrap();
     loop {}
 }
 
